f0b2eaa00c919ff8d01837a8d13d85572149c71c
Vasu Sharma <vsharma.nyc@gmail.com>
2024-07-27 13:45:05 -0400

    added basic model view controller

---
+++ src/controller.js
@@ -0,0 +1,42 @@
+let gameState = {
+    player1: {
+        right: 1,
+        left: 1
+    },
+    player2: {
+        right: 1,
+        left: 1
+    }
+}
+
+const makeMoveP1 = (hand) => {
+    // attack p2 r
+    // attack p2 l
+            // ---> By how much
+
+    alert('p1 move made')
+
+    // swap
+    // --> don't allow the swap both r and l are the 
+}
+
+const makeMoveP2 = () => {
+    // attack p2 r
+    // attack p2 l
+            // ---> By how much
+    alert('p2 move made')
+
+
+    // swap
+    // --> don't allow the swap both r and l are the 
+}
+
+const checkForWin = () => {
+    // is p1 r && l  === 0
+
+    // or is p2 r && l === 0
+} 
+
+document.addEventListener('DOMContentLoaded', () => {
+    // document.body.innerHTML = JSON.stringify(gameState);
+})
